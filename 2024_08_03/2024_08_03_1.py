275dfca3b4153bc8199c5e8c40d38090978333f4
jaylen945 <x1biung@gmail.com>
2024-08-03 11:27:50 +0800

    modify

--- 2024_08_03/2024_08_03_1.py
+++ 2024_08_03/2024_08_03_1.py
@@ -6,14 +6,14 @@ import random
 def created_log_file(file:str,folder:str='data')->str:
     current_path = os.path.abspath(__name__) #取得目前檔案路徑
     directory_name = os.path.dirname(current_path) #取得目前資料夾路徑
-    data_path = os.path.join(directory_name, 'data') #目前資料夾路徑加上data目錄
+    data_path = os.path.join(directory_name, folder) #目前資料夾路徑加上data目錄
     if not os.path.isdir(data_path):
         print(f'沒有{folder}的目錄,手動建立目錄')
         os.mkdir(data_path)
     else:
         print('目錄已經建立')
 
-    log_path = os.path.join(data_path, 'iot.log')
+    log_path = os.path.join(data_path, file)
     if not os.path.isfile(log_path):
         print(f'沒有{file}檔,建立新檔')
         with open(log_path,mode='w', encoding='utf-8',newline='') as file:
