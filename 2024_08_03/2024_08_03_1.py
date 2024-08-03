1ae25b03c0e2d8a2ec6c5f72698b6c31dcfb55e0
jaylen945 <x1biung@gmail.com>
2024-08-03 11:19:03 +0800

    modify

--- 2024_08_03/2024_08_03_1.py
+++ 2024_08_03/2024_08_03_1.py
@@ -33,7 +33,7 @@ def record_info(log_path):
 
 def main():
     now = datetime.now()
-    current_file_name = now.strftime('%Y_%m_%d')
+    current_file_name = now.strftime('%Y_%m_%d.log')
     log_path = created_log_file(folder = 'data', file = current_file_name)
     record_info(log_path)
 
