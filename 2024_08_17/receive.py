922cd7056860b184c5cd62729cb05c22258ba49f
jaylen945 <x1biung@gmail.com>
2024-08-17 14:00:23 +0800

    modify

--- 2024_08_17/receive.py
+++ 2024_08_17/receive.py
@@ -1,10 +1,15 @@
 import paho.mqtt.client as mqtt
+import redis
+
+redis_conn = redis.Redis(host='localhost', port = 6379, password = 'raspberry')
+
 
 def on_message(mosq, obj, msg):
     topic = msg.topic
     message = msg.payload.decode('utf-8')  #接收需要解碼
+    redis_conn.rpush(topic,message)
     print(f'topic={topic}, message:{message}')  #注意大括號
-
+    
 
 if __name__ == '__main__':  #以下是老師新寫法
     client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
