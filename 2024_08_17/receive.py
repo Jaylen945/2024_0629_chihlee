2939218102870d2606d5b53d37eed06233200369
jaylen945 <x1biung@gmail.com>
2024-08-17 13:23:09 +0800

    modify

--- 2024_08_17/receive.py
+++ 2024_08_17/receive.py
@@ -2,11 +2,11 @@ import paho.mqtt.client as mqtt
 
 def on_message(mosq, obj, msg):
     topic = msg.topic
-    message = msg.payload.decode('utf-8')
-    print(f'topic={topic}, message:{message}')
+    message = msg.payload.decode('utf-8')  #接收需要解碼
+    print(f'topic={topic}, message:{message}')  #注意大括號
 
 
-if __name__ == '__main__':
+if __name__ == '__main__':  #以下是老師新寫法
     client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
     client.on_message = on_message
     client.connect('127.0.0.1')
