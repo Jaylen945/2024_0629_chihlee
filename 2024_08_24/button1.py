f46d23719020944f5faabcb3a2208d439e7173d3
jaylen945 <x1biung@gmail.com>
2024-08-24 11:14:52 +0800

    modify

--- 2024_08_24/button1.py
+++ 2024_08_24/button1.py
@@ -1,5 +1,5 @@
 import signal
-from gpiozero import Button, LED
+from gpiozero import Button,LED
 from datetime import datetime
 import paho.mqtt.publish as publish
 import os
@@ -7,31 +7,30 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def user_release():
-    print('使用者按下放開')
+    print("使用者按下放開")
     led.toggle()
     now = datetime.now()
     now_str = now.strftime('%Y-%m-%d %H:%M:%S')
     print(now_str)
-    if led.is_lit:   #前後用三個單引號用連結長句子
-        message = f'''{{   
+    if led.is_lit:
+        message = f'''{{
             "status":true,
             "date":{now_str},
             "topic":"501教室/老師桌燈"
         }}'''
         print(message)
-        publish.single(topic = '501教室/老師桌燈', payload = message, hostname = '127.0.0.1', qos=2)
-        
+        publish.single(topic='501教室/老師桌燈',payload=message,hostname='127.0.0.1',qos=2,auth={'username':os.environ['MQTT_USERNAME'],'password':os.environ['MQTT_PASSWORD']})
     else:
-        message = f'''{{  
+        message = f'''{{
             "status":false,
             "date":{now_str},
             "topic":"501教室/老師桌燈"
-         }}'''
+        }}''' 
         print(message)
-        publish.single(topic = '501教室/老師桌燈', payload = message, hostname = '127.0.0.1', qos=2)
+        publish.single(topic='501教室/老師桌燈',payload=message,hostname='127.0.0.1',qos=2,auth={'username':os.environ['MQTT_USERNAME'],'password':os.environ['MQTT_PASSWORD']})
 
 if __name__ == '__main__':
     button = Button(pin=18)
     button.when_released = user_release
-    led = LED(pin=25)
+    led = LED(pin=25) 
     signal.pause()
--- 2024_08_24/receive.py
+++ 2024_08_24/receive.py
@@ -4,21 +4,21 @@ from dotenv import load_dotenv
 import os
 load_dotenv()
 
-
-redis_conn = redis.Redis(host=os.environ['REDIS_HOST'], port = 6379, password = os.environ['REDIS_PASSWORD'])
+redis_conn = redis.Redis(host=os.environ['REDIS_HOST'], port=6379,password=os.environ['REDIS_PASSWORD'])
 render_redis_conn = redis.Redis.from_url(os.environ['RENDER_REDIS'])
+
 
 def on_message(mosq, obj, msg):
     topic = msg.topic
-    message = msg.payload.decode('utf-8')  #接收需要解碼
+    message = msg.payload.decode('utf-8')
     redis_conn.rpush(topic,message)
     render_redis_conn.rpush(topic,message)
-    print(f'topic={topic}, message:{message}')  #注意大括號
-    
+    print(f"topic={topic},message:{message}")
 
-if __name__ == '__main__':  #以下是老師新寫法
+if __name__ == '__main__':
     client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+    client.username_pw_set(username=os.environ['MQTT_USERNAME'],password=os.environ['MQTT_PASSWORD'])
     client.on_message = on_message
     client.connect(os.environ['MQTT_SERVER'])
-    client.subscribe('501教室/老師桌燈', qos = 2)
+    client.subscribe('501教室/老師桌燈',qos=2)
     client.loop_forever()
